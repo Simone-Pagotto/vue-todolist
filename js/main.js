9741785d44fa47ce557c78d71d29f75513c2bbc2
Simone <simone.pagotto.kg@gmail.com>
2020-11-18 16:39:55 +0100

    try bonus

--- js/main.js
+++ js/main.js
@@ -3,14 +3,20 @@ const todolistApp = new Vue ({
   data: {
     messageArray: [],
     message: '',
+    selectFlagArray: []
   },
   methods: {
     addAction : function(){
       this.messageArray.push(this.message);
+      this.selectFlagArray.push(false);
     },
     removeMessage : function(delIndex){
       console.log(this.delIndex);
       this.messageArray.splice(delIndex,1);
+      this.selectFlagArray.splice(delIndex,1);
+    },
+    selectItem: function(delIndex){
+      this.selectFlagArray[delIndex] = !(this.selectFlagArray[delIndex]);
     }
   }
 })
